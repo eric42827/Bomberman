618eaac3ccf72620e334cc6f3d97203df3b94181
MikeWang <35629082+MikeWangtc@users.noreply.github.com>
2020-06-01 14:59:29 +0800

    game room

---
+++ Assets/Scripts/CharSceneHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+class CharSceneHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public int char_id = 0;
+    public string usr_name;
+    public List<Sprite> sprites;
+
+    void Start(){
+        GameObject.Find("InputName").GetComponent<InputField>().characterLimit = 7;
+        setBtnSprite();
+    }
+    public void Update(){
+        DisplayName();
+    }
+    public void OnSelectChar(int idx){
+        Debug.Log("In OnSelectChar");
+        char_id = idx;
+        GameObject.Find("ChosenChar").GetComponent<Image>().sprite = sprites[char_id];
+    }
+    public void OnRegister(){
+        Debug.Log("In OnRegister");
+        // StaticClass.setPlayerInfo(usr_name, char_id);
+        SceneManager.LoadScene("GameRoom");
+    }
+    public void DisplayName(){
+        usr_name = GameObject.Find("InputName").GetComponent<InputField>().text;
+        GameObject.Find("DisplayName").GetComponent<Text>().text = usr_name;
+    }
+    public void setBtnSprite(){
+        for (int i = 0 ; i < 15 ; ++i){
+            string btn = "Button"+i;
+            GameObject.Find(btn).GetComponent<Image>().sprite = sprites[i];
+        }
+    }
+}
---
+++ Assets/Scripts/RoomSceneHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RoomSceneHandler : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public List<Sprite> sprites;
+    public List<Text> players_name;
+    public List<int> players_id;
+    void Start(){
+        if(!isServer){
+            HideStartBtn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHostStartGame(){
+        
+    }
+    private void HideStartBtn(){
+        GameObject.Find("Start").SetActive(false);
+    }
+}
